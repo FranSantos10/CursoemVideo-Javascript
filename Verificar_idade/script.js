dff659eb3e0e15ef0a4e4ae37227309f17c63c20
FranSantos10 <fran.hagelin@gmail.com>
2023-03-06 11:26:55 -0300

    Adicionando as imagens de cada genero e idade - Verificador de idade

--- Verificar_idade/script.js
+++ Verificar_idade/script.js
@@ -13,15 +13,45 @@ function verificar(){
         var sex = document.getElementsByName('rdsex')
         var idade = ano - Number(anouser.value)
         var genero = ''
+
+        //criando imagem e informando id
         var img =document.createElement('img')
         img.setAttribute('id', 'foto')
-        
+       
+        //genero
         if (sex[0].checked) {
-            genero = 'homem'            
+            genero = 'homem'     
+            if (idade >=0 && idade <10) {
+                //criança
+                img.setAttribute('src', '../img/bb_m.png')
+            } else if(idade <21){
+                //jovem
+                img.setAttribute('src', '../img/jovem_m.png')
+            } else if(idade <50){
+                //adulto 
+                img.setAttribute('src', '../img/adulta_m.png')
+            } else{
+                //idoso 
+                img.setAttribute('src', '../img/idoso_m.png')
+            }   
         }else if(sex[1].checked){
             genero ='mulher'
+            if (idade >=0 && idade <10) {
+                //criança
+                img.setAttribute('src', '../img/bb_f.png')
+            } else if(idade <21){
+                //jovem
+                img.setAttribute('src', '../img/jovem_f.png')
+            } else if(idade <50){
+                //adulto 
+                img.setAttribute('src', '../img/adulta_f.png')
+            } else{
+                //idoso 
+                img.setAttribute('src', '../img/idoso_f.png')
+            } 
         }
         res.style.textAlign= 'center'
-        res.innerHTML = `${genero} e idade ${idade} anos` 
+        res.innerHTML = `Detectamos o ${genero} e idade ${idade} anos` 
+        res.appendChild(img)
     }
 }
