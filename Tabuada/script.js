f4540c27ed358bdfe54b756850bb405ef0af89d9
FranSantos10 <fran.hagelin@gmail.com>
2023-03-09 20:02:24 -0300

    Criado o laço de repeticao exercicio Tabuada

--- Tabuada/script.js
+++ Tabuada/script.js
@@ -1,4 +1,27 @@
 function gerarTabuada(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
+    
+    //verifica se o campo esta vazio
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um numero')
+    } else {
+        //converte em numero o valor informado
+        let n = Number(num.value)
+       // let c = 1 // contador
+        tab.innerHTML = ''
+       /* while (c <=10 ) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }*/
+        
+        for(let c=1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+        }
+    }
+    
 }
